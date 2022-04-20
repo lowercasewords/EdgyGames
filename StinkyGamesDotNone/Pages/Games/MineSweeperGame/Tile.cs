5328852e56e98068c8287b1d3328b6a78d7d6ec5
lowercasewords <87104601+lowercasewords@users.noreply.github.com>
2022-04-20 14:28:56 -0700

    starter code

---
+++ StinkyGamesDotNone/Pages/Games/MineSweeperGame/Tile.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace StinkyGamesDotNone.Pages.Games.MineSweeperGame
+{
+    public class Tile
+    {
+        private static int muiltiton = 0;
+        //public const string _empty = "empty";
+        //public const string _mine = "mine";
+        //public const string _disarmedMine = "disarmedMine";
+        //public const
+
+        public Tile()
+        {
+            muiltiton++;
+        }
+    }
+}
---
+++ StinkyGamesDotNone/Pages/Games/SudokuGame/Sudoku.cshtml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace StinkyGamesDotNone.Pages.Games
+{
+    public class SudokuModel : PageModel
+    {
+        public Map _map;
+
+        //public event EventHandler Click;
+
+        public void OnPostRestart()
+        {
+            _map.RestartGrids();
+        }
+        public void OnGet()
+        {
+            _map = new Map();
+        }
+
+        public void OnSet()
+        {
+
+        }
+    }
+}

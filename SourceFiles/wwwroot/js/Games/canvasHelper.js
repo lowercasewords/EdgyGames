ecccc92dd22716b7ca02503e19381411b424d9a8
lowercasewords <87104601+lowercasewords@users.noreply.github.com>
2022-08-17 10:49:47 -0700

    reszie added

--- SourceFiles/wwwroot/js/Games/canvasHelper.js
+++ SourceFiles/wwwroot/js/Games/canvasHelper.js
@@ -12,7 +12,6 @@ export class CanvasObj {
 
         /**
          * Checks if your point is within this object
-         * @param {Object} shape with
          * @param {*} pointX
          * @param {*} pointY
          * @returns whether or not specified coordinate hit the shape
@@ -25,38 +24,49 @@ export class CanvasObj {
 }
  /**
  * Extends a Canvas Class, additionally having 
- * render properties with custom default color
+ * render properties with custom default style
  * @param {Number} size size (width and height)
  * @param {Number} x x-coordinate
  * @param {Number} y y-coordinate
- * @param {String} outlineColor default outline color
- * @param {String} fillColor default fill color
+ * @param {String} outlineStyle default outline style
+ * @param {String} fillStyle default fill style
  */
-export class ColorCanvasObj extends CanvasObj{
-    constructor(x, y, size, outlineColor, fillColor) {
+export class StyleCanvasObj extends CanvasObj{
+    constructor(x, y, size) {
         super(x, y, size);
-        this.fillColor = fillColor;
-        this.outlineColor = outlineColor;
-        this.linesize = 5;
-        /**
-         * Fills up the current canvas object with color
-         * @param {Object} context the context to paint with
-         * @param {String} color the color of the fill
-         */
-        this.fill = (context, color = this.fillColor) => {
-            context.fillStyle = color;
-            context.fillRect(this.x, this.y, this.size, this.size);
-        };
-        /**
-         * Outlines the 
-         * @param {String} color 
-         */
-        this.outline = (context, color = this.outlineColor) => {
-            context.linesize = this.linesize;
-            context.strokeStyle = color;
-            context.strokeRect(this.x, this.y, this.size, this.size);
-        };
+    };
+    /**
+     * Fills up the current canvas object with custom style
+     * @param {CanvasRenderingContext2D} context the 2d context to paint with
+     * @param {String} style the style of the fill
+     */
+    fill = (context, style = undefined) => {
+        if(style == undefined) {
+            context.clearRect(this.x, this.y, this.size, this.size);
+            return;
+        }
+        this.saveStyle(context);
+        context.fillStyle = style;
+        context.fillRect(this.x, this.y, this.size, this.size);
+        this.releaseStyle(context);
+    };
+    /**
+     * Outlines the current canvas object with custom style
+     * @param {CanvasRenderingContext2D}
+     * @param {String} style 
+     */
+    outline = (context, style = undefined) => {
+        if(style == undefined) {
+            context.clearRect(this.x, this.y, this.size, this.size);
+            return;
+        }
+        this.saveStyle(context);
+        context.strokeStyle = style;
+        context.strokeRect(this.x, this.y, this.size, this.size);
+        this.releaseStyle(context);
     }
+    saveStyle = (context) => this.savedStyle = context.fillStyle;
+    releaseStyle = (context) => context.fillStyle = this.savedStyle;
 }
 
 /**
@@ -66,7 +76,7 @@ export class ColorCanvasObj extends CanvasObj{
  */
 export function resize(canvas, rescale) {
     canvas.height = canvas.offsetHeight;
-    canvas.size = canvas.offsetsize;
+    canvas.width = canvas.offsetWidth;
     rescale();
-    console.log(`canvas height: ${canvas.height}\ncanvas size: ${canvas.size}`);
+    console.log(`canvas height: ${canvas.height}\ncanvas width: ${canvas.width}`);
 }
